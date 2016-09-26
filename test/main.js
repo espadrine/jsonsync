44d366c00ae050062c06483da58d2a082bbe813b
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-09-26 21:49:23 +0200

    Test concurrent additions and removals
    Also ensure that our simulated network nodes have distinct machine ids and are
    created with all nodes already connected.

--- test/main.js
+++ test/main.js
@@ -10,10 +10,13 @@ function setup(n, value) {
 
   for (var i = 0; i < n; i++) {
     network.push(networks.addNode())
+  }
+  // We separate the addition of node so that we needn't send connect events.
+  for (var i = 0; i < n; i++) {
     node.push(new JsonSync({
       network: network[i],
       value: JsonSync.cloneValue(value),
-      machine: [0]}))
+      machine: [i]}))
   }
 
   return {
@@ -30,4 +33,24 @@ var node = situation.node
 
 node[0].add('/hello', 'world')
 networks.flush(0, 1)
-assert.equal(node[1].content.hello, 'world')
+assert.equal(node[1].content.hello, 'world',
+  'One-way transmission of addition')
+
+node[1].remove('/hello')
+networks.flush(1, 0)
+assert.equal(node[1].content.hello, undefined,
+  'One-way transmission of removal')
+
+node[0].add('/concurrent', 'update')
+node[1].add('/concurrent', 'change')
+networks.flush(0, 1)
+networks.flush(1, 0)
+assert.equal(node[0].content.concurrent, 'change',
+  'Concurrent two-way addition')
+
+node[0].remove('/concurrent')
+node[1].remove('/concurrent')
+networks.flush(0, 1)
+networks.flush(1, 0)
+assert.equal(node[0].content.concurrent, undefined,
+  'Concurrent two-way removal')
